495d0c2127bab129630c04b2f39ba73a8e08da97
Dan Hancox <dthancox@gmail.com>
2023-02-02 20:43:28 +0000

    Prettier installed

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,14 @@
-
 // import { useState } from 'react';
-import '../styles/search.css';
+import "../styles/search.css";
 
-import React from 'react'
+import React from "react";
 
 const Search = () => {
   return (
     <>
-      render to screen Dvf
+      <h1 className="input">The Moon</h1>
     </>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
